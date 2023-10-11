e424386f3a38315db6c537f32a5d9302de29f144
harddownloader <serafimdev@gmail.com>
2023-10-11 18:07:35 +0300

    added sentry

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import * as Sentry from "@sentry/node";
+import { ProfilingIntegration } from "@sentry/profiling-node";
 import express from "express";
 import logger from "morgan";
 import * as path from "path";
@@ -9,6 +11,28 @@ import { index } from "./routes/index";
 // Create Express server
 export const app = express();
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Sentry.Integrations.Express({ app }),
+        new ProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
@@ -17,7 +41,10 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
+
 app.use("/", index);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(errorNotFoundHandler);
 app.use(errorHandler);
